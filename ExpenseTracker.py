29016a78d3942db5aff78bc893ad1ea0bb6b4d0b
yoel <yoelkaleb@yahoo.com>
2026-04-04 17:44:52 -0700

    Completed load_data and improved code readability.
    Functional Minimal, Low Target, and Desirable Target have now all been completed.

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -1,5 +1,4 @@
 ''' Solo Project: Personal Expense and Budget Tracker '''
-import csv
 from datetime import datetime
 
 # Expense class
@@ -19,7 +18,6 @@ print("\tWelcome to your Personal Expense and Budget Tracker\n")
 continue_program = True # Decides when to terminate program
 
 # Vector that holds all expenses
-## expenses = [] for test
 expenses = [
     Expense(12.50, "food", datetime(2026, 3, 25), "lunch"),
     Expense(45.00, "gas", datetime(2026, 3, 24), "fuel"),
@@ -40,8 +38,10 @@ def menu():
     print("7. Calculate Total Expenses")
     print("8. Exit")
     while True:
+
         try:
             return int(input('\nEnter your choice: '))
+
         except ValueError:
             print('Invalid choice, enter a number from 1 to 8.\n')
 
@@ -61,32 +61,44 @@ def display_header_helper():
     print("-" * 70)
 
 # Add Expense Function
-def add_expense():
+def add_expense(expenses):
     # Enter 'amount'
     while True:
+
         try:
             amount = float(input('Enter your amount: '))
             if amount <= 0:
                 print('Amount must be greater than 0.')
                 continue
+
             break
+
         except ValueError:
             print('Invalid amount, try again.')
 
     # Enter 'category' - Removes whitespaces
-    category = input('Enter your category: ').strip()
+    category = input('Enter your category: ').strip().lower()
 
     # Enter 'date' - Use string parse time to translate into a date type
     while True:
+
         date_str = input('Enter date (YYYY-MM-DD): ').strip()
         try:
             date = datetime.strptime(date_str, "%Y-%m-%d")
             break
+
         except ValueError:
             print('Invalid date, try again.')
 
     # Enter 'description' - Removes whitespaces
-    description = input('Enter description: ').strip()
+    while True:
+        description = input('Enter description (do not include commas): ').strip()
+
+        if ',' in description:
+            print('Description cannot contain commas.')
+            continue
+
+        break
 
     # Creates a new Expense instance with 'amount', 'category', and 'date'
     new_expense = Expense(amount, category, date, description)
@@ -96,55 +108,59 @@ def add_expense():
     print('Expense added successfully.\n')
 
 # View Expense Function
-def view_expense():
+def view_expenses(expenses):
     if not expenses:
         print('\nNo expenses recorded.\n')
-        input("Press enter to continue...\n")
+        input("Press enter to continue...")
+
     else:
         print('\nExpenses:')
-        # Header
-        display_header_helper()        
 
-        # Rows
-        display_expenses_helper(expenses)
+        display_header_helper()             # Header        
+        display_expenses_helper(expenses)   # Rows
 
     print()  # \n
 
 # Delete Expense Function
-def delete_expense():
+def delete_expense(expenses):
     if not expenses:
         print('No expenses recorded.\n')
-        input("Press enter to continue...\n")
+        input("Press enter to continue...")
+
     else:
         print('Select which expense number you would like to remove.')
-        # Header
-        display_header_helper()        
-        display_expenses_helper(expenses)
+        display_header_helper()             # Header
+        display_expenses_helper(expenses)   # Rows
+
         while True:
+
             try:
                 delete_expense_index = int(input('Remove: ')) - 1
+
                 if 0 <= delete_expense_index < len(expenses):
                     del expenses[delete_expense_index]
                     print('Expense removed successfully.\n')
                     break
+
                 else:
                     print('Invalid choice, try again. Please select a number from the item list.')
+
             except ValueError:
                 print('Invalid choice, try again. Please select a number from the item list.')
 
 # Update Expense Function
-def update_expense():
+def update_expense(expenses):
     if not expenses:
         print('No expenses recorded.\n')
-        input("Press enter to continue...\n")
+        input("Press enter to continue...")
+
     else:
         print('Select which expense number you would like to update.')
-        # Header
-        display_header_helper()        
-        # Rows
-        display_expenses_helper(expenses)
+        display_header_helper()                # Header
+        display_expenses_helper(expenses)   # Rows
 
         while True:
+
             try:
                 update_expense_index = int(input('Update: ')) - 1
                 if 0 <= update_expense_index < len(expenses):
@@ -163,10 +179,13 @@ def update_expense():
                     )
 
                     valid_fields = ['amount', 'category', 'date', 'description']
+
                     field_to_update = str(input('Selection: ')).strip().lower()
+
                     if field_to_update in valid_fields:
                         if field_to_update == 'amount':
                             while True:
+
                                 try:
                                     amount = float(input('Enter your amount: '))
                                     if amount <= 0:
@@ -175,14 +194,20 @@ def update_expense():
                                     break
                                 except ValueError:
                                     print('Invalid amount, try again.')
+
                             expenses[update_expense_index].amount = amount
+
                             print('Expense updated successfully.\n')
                             break
+
                         elif field_to_update == 'category':
-                            category = input('Enter your category: ').strip()
+                            category = input('Enter your category: ').strip().lower()
+
                             expenses[update_expense_index].category = category
+
                             print('Expense updated successfully.\n')
                             break
+
                         elif field_to_update == 'date':
                             while True:
                                 date_str = input('Enter date (YYYY-MM-DD): ').strip()
@@ -191,22 +216,35 @@ def update_expense():
                                     break
                                 except ValueError:
                                     print('Invalid date, try again.')
+
                             expenses[update_expense_index].date = date
+
                             print('Expense updated successfully.\n')
                             break
+
                         elif field_to_update == 'description':
-                            description = input('Enter your description: ').strip()
+                            while True:
+                                description = input('Enter description (do not include commas): ').strip()
+                                if ',' in description:
+                                    print('Description cannot contain commas.')
+                                    continue
+                                break
+
                             expenses[update_expense_index].description = description
+
                             print('Expense updated successfully.\n')
                             break
+
                     else:
                         print(f'Invalid choice, try again. Please select a valid field {valid_fields}.') # Checks valid field (Amount | Category | Date | Description)
+
                 else:
                     print('Invalid choice, try again. Please select a number from the item list.') # Checks valid index for budget item in expenses[]
+
             except ValueError:
                 print('Invalid choice, try again. Please select a number from the item list.') # Checks valid index TYPE for budget item in expenses[]
 
-# Save/Load Data Functions
+# Save Data Function
 def save_data(expenses):
     try:
         with open('expenses.txt', 'w') as file:
@@ -217,13 +255,63 @@ def save_data(expenses):
                     f"{expense.date.strftime('%Y-%m-%d')},"
                     f"{expense.description}\n"
                 )
+
         print('Save successful.\n')
+
     except Exception:
         print('An error occurred while saving data.\n')
 
+# Load Data Function
 def load_data(expenses):
-    pass
-    # PICK UP HERE # 1. Ask for file name to be saved as. Create (open) the new file and save data.
+    file_name = input('Enter file name: ')
+    try:
+        with open(file_name, 'r') as file:
+
+            expenses.clear()
+
+            for line in file:
+                line = line.strip()
+
+                if not line: # same as: if line == ""
+                    continue
+
+                parts = line.split(',')
+
+                if len(parts) != 4:
+                    print(f"Invalid file format.\n")
+                    continue
+
+                try:
+                    amount = float(parts[0])
+                    category = parts[1]
+                    date = datetime.strptime(parts[2], "%Y-%m-%d")
+                    description = parts[3]
+
+                    expenses.append(Expense(amount, category, date, description))
+
+                except ValueError:
+                    print(f"Error parsing line: {line}")
+
+        print("Load successful.\n")
+
+    except FileNotFoundError:
+        print('File not found.\n')
+
+    except Exception:
+        print('An error occurred while loading data.\n')
+        
+# Calculate Total Expenses Function
+def total_expenses(expenses):
+    total = 0
+    if not expenses:
+        print('No expenses found.\n')
+        input("Press enter to continue...")
+        return
+
+    for expense in expenses:
+        total += expense.amount
+
+    print(f'Total expenses: ${total:.2f}\n')
 
 # MAIN
 while continue_program:
@@ -231,29 +319,37 @@ while continue_program:
     choice = menu()
 
     if choice == 1:     # 1. Add Expense (Progressive Goal: Layer 1 - Functional Minimal)
-        add_expense()
-        input("Press enter to continue...\n")
+        add_expense(expenses)
+        input("Press enter to continue...")
 
     elif choice == 2:   # 2. View Expenses (Progressive Goal: Layer 1 - Functional Minimal)
-        view_expense()
-        input("Press enter to continue...\n")
+        view_expenses(expenses)
+        input("Press enter to continue...")
 
     elif choice == 3:   # 3. Delete Expense (Progressive Goal: Layer 2 - Low Target)
-        delete_expense()
-        input("Press enter to continue...\n")
+        delete_expense(expenses)
+        input("Press enter to continue...")
 
     elif choice == 4:   # 4. Update Expense (Progressive Goal: Layer 2 - Low Target)
-        update_expense()
-        input("Press enter to continue...\n")
+        update_expense(expenses)
+        input("Press enter to continue...")
 
     elif choice == 5:   # 5. Save Data (Progressive Goal: Layer 1 - Functional Minimal)
-        pass
+        save_data(expenses)
+        input("Press enter to continue...")
+
     elif choice == 6:   # 6. Load Data (Progressive Goal: Layer 1 - Functional Minimal)
-        pass
+        load_data(expenses)
+        input("Press enter to continue...")
+        
     elif choice == 7:   # 7. Calculate Total Expenses (Progressive Goal: Layer 3 - Desirable Target)
-        pass
+        total_expenses(expenses)
+        input("Press enter to continue...")
+
     elif choice == 8:   # 8. Quit
+        print("Program successfully terminated. Thank you!")
         continue_program = False
+
     else:
         print("Invalid choice, try again.\n")
 
